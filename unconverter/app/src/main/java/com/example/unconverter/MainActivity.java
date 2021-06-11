42b437080e029607b239bba0421ab8d6604fbdc3
saloni <202012073@daiict.ac.in>
2021-06-11 22:28:04 +0530

    v1

---
+++ unconverter/app/src/main/java/com/example/unconverter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.unconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
+        editText=findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+                String kg=editText.getText().toString();
+                int num=Integer.parseInt(kg);
+                double pound=num*2.20462;
+                textView.setText("Pounds: "+pound);
+            }
+
+        });
+    }
+}
